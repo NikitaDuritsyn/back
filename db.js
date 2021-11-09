a5b0a6d56c5ef1535032cd7e779d978aac77ef2b
KiTKasatka <90504295+NikitaDuritsyn@users.noreply.github.com>
2021-11-09 14:07:07 +0300

    Я хз как но должно же работать

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: "NikitaDuritsyn",
+    user: "NikitaDuritsynasdasd",
     password: "123123",
     host: "localhost",
     port: 5432,
